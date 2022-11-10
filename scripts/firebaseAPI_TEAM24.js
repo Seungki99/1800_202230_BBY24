81fd84a70d6b65a251a04635f9faf7d1336ba734
Yuvraaj <yuvraajchouhan.79@gmail.com>
2022-11-10 08:45:12 -0800

    sprint #2 almost done.

--- scripts/firebaseAPI_TEAM24.js
+++ scripts/firebaseAPI_TEAM24.js
@@ -2,14 +2,13 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 const firebaseConfig = {
-    apiKey: "AIzaSyD_Zmbil8zYAHlyBSYpfxb3k0oPR1EVKkU",
-    authDomain: "myspot-fe819.firebaseapp.com",
-    projectId: "myspot-fe819",
-    storageBucket: "myspot-fe819.appspot.com",
-    messagingSenderId: "1050032436648",
-    appId: "1:1050032436648:web:142180e95555c43725599d"
-  };
-
+  apiKey: "AIzaSyD_Zmbil8zYAHlyBSYpfxb3k0oPR1EVKkU",
+  authDomain: "myspot-fe819.firebaseapp.com",
+  projectId: "myspot-fe819",
+  storageBucket: "myspot-fe819.appspot.com",
+  messagingSenderId: "1050032436648",
+  appId: "1:1050032436648:web:142180e95555c43725599d"
+};
 //--------------------------------------------
 // initialize the Firebase app
 // initialize Firestore database if using it
