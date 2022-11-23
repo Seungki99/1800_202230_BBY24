60e8f10f258369360d33fc5684a2073a1b291746
Tomasz Stojek <stojek604@gmail.com>
2022-11-23 14:50:31 -0800

    console logs for testing

--- scripts/find.js
+++ scripts/find.js
@@ -1,79 +1,79 @@
-function writeparkingspots() {
-  //define a variable for the collection you want to create in Firestore to populate data
-  var spotsRef = db.collection("parkingspots");
+// function writeparkingspots() {
+//   //define a variable for the collection you want to create in Firestore to populate data
+//   var spotsRef = db.collection("parkingspots");
 
-  spotsRef.add({
-    code: "AB01",
-    available: "true",
-    description: "I really hope this works", //replace with your own city?
-    geolocation: "url string, I don't care",
-    timestamp: "easy",
-    last_updated: firebase.firestore.FieldValue.serverTimestamp(),
-  });
-  spotsRef.add({
-    code: "AB02",
-    available: "true",
-    description: "I really hope this works",
-    geolocation: "url string, I don't care",
-    timestamp: "easy",
-    vehicle: "NF195P",
-    last_updated: firebase.firestore.FieldValue.serverTimestamp(),
-  });
-  spotsRef.add({
-    code: "AB03",
-    available: "true",
-    description: "I really hope this works",
-    geolocation: "url string, I don't care",
-    timestamp: "easy",
-    vehicle: "NF195P",
-    last_updated: firebase.firestore.FieldValue.serverTimestamp(),
-  });
-  spotsRef.add({
-    code: "AB04",
-    available: "true",
-    description: "I really hope this works",
-    geolocation: "url string, I don't care",
-    timestamp: "easy",
-    vehicle: "NF195P",
-    last_updated: firebase.firestore.FieldValue.serverTimestamp(),
-  });
-  spotsRef.add({
-    code: "AB05",
-    available: "true",
-    description: "I really hope this works",
-    geolocation: "url string, I don't care",
-    timestamp: "easy",
-    vehicle: "NF195P",
-    last_updated: firebase.firestore.FieldValue.serverTimestamp(),
-  });
-  spotsRef.add({
-    code: "AB06",
-    available: "true",
-    description: "I really hope this works",
-    geolocation: "url string, I don't care",
-    timestamp: "easy",
-    vehicle: "NF195P",
-    last_updated: firebase.firestore.FieldValue.serverTimestamp(),
-  });
-  spotsRef.add({
-    code: "AB07",
-    available: "true",
-    description: "I really hope this works",
-    geolocation: "url string, I don't care",
-    timestamp: "easy",
-    vehicle: "NF195P",
-    last_updated: firebase.firestore.FieldValue.serverTimestamp(),
-  });
-  spotsRef.add({
-    code: "AB08",
-    available: "true",
-    description: "I really hope this works",
-    geolocation: "Burnaby",
-    timestamp: "easy",
-    vehicle: "NF195P",
-    last_updated: firebase.firestore.FieldValue.serverTimestamp(),
-  });
-}
+//   spotsRef.add({
+//     code: "AB01",
+//     available: "true",
+//     description: "I really hope this works", //replace with your own city?
+//     geolocation: "url string, I don't care",
+//     timestamp: "easy",
+//     last_updated: firebase.firestore.FieldValue.serverTimestamp(),
+//   });
+//   spotsRef.add({
+//     code: "AB02",
+//     available: "true",
+//     description: "I really hope this works",
+//     geolocation: "url string, I don't care",
+//     timestamp: "easy",
+//     vehicle: "NF195P",
+//     last_updated: firebase.firestore.FieldValue.serverTimestamp(),
+//   });
+//   spotsRef.add({
+//     code: "AB03",
+//     available: "true",
+//     description: "I really hope this works",
+//     geolocation: "url string, I don't care",
+//     timestamp: "easy",
+//     vehicle: "NF195P",
+//     last_updated: firebase.firestore.FieldValue.serverTimestamp(),
+//   });
+//   spotsRef.add({
+//     code: "AB04",
+//     available: "true",
+//     description: "I really hope this works",
+//     geolocation: "url string, I don't care",
+//     timestamp: "easy",
+//     vehicle: "NF195P",
+//     last_updated: firebase.firestore.FieldValue.serverTimestamp(),
+//   });
+//   spotsRef.add({
+//     code: "AB05",
+//     available: "true",
+//     description: "I really hope this works",
+//     geolocation: "url string, I don't care",
+//     timestamp: "easy",
+//     vehicle: "NF195P",
+//     last_updated: firebase.firestore.FieldValue.serverTimestamp(),
+//   });
+//   spotsRef.add({
+//     code: "AB06",
+//     available: "true",
+//     description: "I really hope this works",
+//     geolocation: "url string, I don't care",
+//     timestamp: "easy",
+//     vehicle: "NF195P",
+//     last_updated: firebase.firestore.FieldValue.serverTimestamp(),
+//   });
+//   spotsRef.add({
+//     code: "AB07",
+//     available: "true",
+//     description: "I really hope this works",
+//     geolocation: "url string, I don't care",
+//     timestamp: "easy",
+//     vehicle: "NF195P",
+//     last_updated: firebase.firestore.FieldValue.serverTimestamp(),
+//   });
+//   spotsRef.add({
+//     code: "AB08",
+//     available: "true",
+//     description: "I really hope this works",
+//     geolocation: "Burnaby",
+//     timestamp: "easy",
+//     vehicle: "NF195P",
+//     last_updated: firebase.firestore.FieldValue.serverTimestamp(),
+//   });
+// }
 function populateCardsDynamically() {
     let parkingspotCardTemplate = document.getElementById("parkingspotCardTemplate");
     let parkingspotCardGroup = document.getElementById("parkingspotCardGroup");
@@ -85,8 +85,10 @@ function populateCardsDynamically() {
             allSpots.forEach(doc => {
                 var spotDescription = doc.data().description; //gets the name field
                 var spotID = doc.data().code; //gets the unique CODE field
+                console.log("spot id is ", spotID);
                 var spotGeolocation = doc.data().geolocation; //gets the length field
                 let testspotCard = parkingspotCardTemplate.content.cloneNode(true);
+                console.log('the testspotCard is ', testspotCard);
                 testspotCard.querySelector('.card-title').innerHTML = spotDescription;     //equiv getElementByClassName
                 testspotCard.querySelector('.card-length').innerHTML = spotGeolocation;  //equiv getElementByClassName
                 testspotCard.querySelector('a').onclick = () => setHikeData(spotID);//equiv getElementByTagName
@@ -99,5 +101,6 @@ function populateCardsDynamically() {
 populateCardsDynamically();
 
 function setSpotData(id){
+  console.long('the id spot id', id)
     localStorage.setItem ('spotID', id);
 }
