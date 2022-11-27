b9ce7e58990586ad66e2b57331d6d63895bd2a79
Tomasz Stojek <stojek604@gmail.com>
2022-11-27 15:49:47 -0800

    changed the myspots population

---
+++ scripts/mySpots.js
@@ -0,0 +1,42 @@
+//firebase.auth().onAuthStateChanged(user => {
+//   if (user) {
+//       getBookmarks(user)// calls the function if signed in
+//   } else {
+//       console.log("No user is signed in");
+//   }
+//});
+
+function populateCardsDynamically() {
+    let parkingspotCardTemplate = document.getElementById("parkingspotCardTemplate");
+    let parkingspotCardGroup = document.getElementById("parkingspotCardGroup");
+    let currentDate = Date();
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            var userID = user.uid;
+            console.log(currentDate);
+            db.collection("parkingspots")
+                .where(("renterID", "==", userID) && "date", ">=", currentDate).get()
+                .then(allSpots => {
+                    allSpots.forEach(doc => {
+                        var spotDescription = doc.data().description; //gets the name field
+                        var spotID = doc.data().code; //gets the unique CODE field
+                        var spotGeolocation = doc.data().geolocation; //gets the length field
+                        var spotAvailable = doc.data().available;
+                        let testspotCard = parkingspotCardTemplate.content.cloneNode(true);
+                        testspotCard.querySelector('.card-available').innerHTML = spotAvailable;
+                        testspotCard.querySelector('.card-title').innerHTML = spotDescription; //equiv getElementByClassName
+                        testspotCard.querySelector('.card-length').innerHTML = spotGeolocation; //equiv getElementByClassName
+                        testspotCard.querySelector('a').onclick = () => setHikeData(spotID); //equiv getElementByTagName
+                        testspotCard.querySelector('img').src = `./images/${spotID}.jpg`; //equiv getElementByTagName
+                        parkingspotCardGroup.appendChild(testspotCard);
+                    })
+
+                })
+        }
+    })
+}
+populateCardsDynamically();
+
+function setSpotData(id) {
+    localStorage.setItem('spotID', id);
+}
