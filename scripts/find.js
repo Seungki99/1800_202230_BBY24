b1e2fad87963592176e5fc3768c7b592e0351c04
Seungki Hong <tmdrl9913@gmail.com>
2022-12-01 00:00:21 -0800

    name in find.html

--- scripts/find.js
+++ scripts/find.js
@@ -1,3 +1,22 @@
+function insertName(){
+  // to check if the user is logged in:
+   firebase.auth().onAuthStateChanged(user =>{
+       if (user){
+           console.log(user.uid); // let me to know who is the user that logged in to get the UID
+          currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
+          currentUser.get().then(userDoc=>{
+              //get the user name
+              var user_Name= userDoc.data().name;
+              console.log(user_Name);
+              $("#name-goes-here").text(user_Name); //jquery
+              // document.getElementByID("name-goes-here").innetText=user_Name;
+          })    
+      }
+  
+   })
+  }
+  insertName();
+
 function populateCardsDynamically() {
     let parkingspotCardTemplate = document.getElementById("parkingspotCardTemplate");
     let parkingspotCardGroup = document.getElementById("parkingspotCardGroup");
